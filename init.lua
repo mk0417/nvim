3eb11862113b0a5369a6bfc29132171de53a8266
Peng <pengdotli@gmail.com>
2023-11-28 18:03:55 +0000

    update

--- init.lua
+++ init.lua
@@ -13,3 +13,17 @@ require('defaults')
 require('keybindings')
 
 print('Hello Peng')
+
+if vim.g.neovide then
+    vim.o.guifont = "Iosevka Comfy:h12"
+
+    vim.g.neovide_padding_top = 0
+    vim.g.neovide_padding_bottom = 0
+    vim.g.neovide_padding_right = 0
+    vim.g.neovide_padding_left = 0
+
+    vim.g.neovide_cursor_animation_length = 0
+    vim.g.neovide_cursor_vfx_mode = ""
+    vim.g.neovide_cursor_antialiasing = false
+    vim.g.neovide_cursor_animate_command_line = false
+end
--- lua/config/latex.lua
+++ lua/config/latex.lua
@@ -7,6 +7,7 @@ return {
 
         g.vimtex_view_enabled = 1
         g.vimtex_view_method = 'skim'
+        g.vimtex_compiler_latexmk = { options = { '-shell-escape' } }
 
         map('n', ';tt', ':VimtexCompile<CR>', {})
         map('n', ';tv', ':VimtexView<CR>', {})
